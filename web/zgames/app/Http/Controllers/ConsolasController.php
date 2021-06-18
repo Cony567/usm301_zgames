9e43145d30fcc031ac682d93c420139e9b002c65
Connor <cony.castillo03@gmail.com>
2021-06-17 20:37:57 -0400

    Generar tabla en la base de datos y generamos modelos y controladores

--- web/zgames/app/Http/Controllers/ConsolasController.php
+++ web/zgames/app/Http/Controllers/ConsolasController.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Consola;
 use Illuminate\Http\Request;
+
 
 class ConsolasController extends Controller
 {
@@ -10,4 +11,21 @@ class ConsolasController extends Controller
         $marcas = ["Sony","Microsoft","Nintendo","Sega"];
         return $marcas;
     }
+
+
+    public function getConsolas(){
+        $consolas = Consola::all();
+        return $consolas;
+    }
+
+    public function crearConsola(){
+        $consola = new Consola();
+        $consola->nombre = "Nintendo Switch";
+        $consola->marca = "Nintendo";
+        $consola->anio = 2015;
+
+        $consola->save();
+        return $consola;
+    }
+
 }
